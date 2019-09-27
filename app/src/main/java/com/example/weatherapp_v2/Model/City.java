0c3d84d67a793c9a61d351673043ff9d0d015b52
Luc Levez <r0781150@student.vives.be>
2019-09-27 17:13:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/weatherapp_v2/Model/City.java
@@ -0,0 +1,10 @@
+package com.example.weatherapp_v2.Model;
+
+public class City {
+    public int id;
+    public String name;
+    public Coord coord;
+    public String country;
+
+
+}
---
+++ app/src/main/java/com/example/weatherapp_v2/Model/Rain.java
@@ -0,0 +1,4 @@
+package com.example.weatherapp_v2.Model;
+
+public class Rain {
+}
